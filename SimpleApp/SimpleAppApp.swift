98be4a5913687bd608b090af6f9e16744889e1cd
ZephyrYWZhou <91304878+ZephyrYWZhou@users.noreply.github.com>
2022-12-15 12:35:52 -0600

    Initial Commit

---
+++ SimpleApp/SimpleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleAppApp.swift
+//  SimpleApp
+//
+//  Created by Zhou Yuewei on 12/15/22.
+//
+
+import SwiftUI
+
+@main
+struct SimpleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
